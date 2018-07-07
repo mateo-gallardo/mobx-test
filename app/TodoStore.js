2534263963330cdfba3470c8a0c29fecac1e7156
Mateo Gallardo <mateogallardo@gmail.com>
2018-07-07 17:07:26 -0300

    8469eef but using mobx tools

--- app/TodoStore.js
+++ app/TodoStore.js
@@ -1,37 +1,36 @@
 import { observable, computed } from 'mobx';
 
+const arrayEquals = (newArray, oldArray) => {
+    if (newArray.length == oldArray.length) {
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i] != oldArray[i]) {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
 class TodoStore {
     @observable todos = ['buy milk', 'buy eggs'];
     @observable filter = '';
-    
-    previousFilteredTodos = [];
-    @computed get filteredTodos() {
+
+    /**
+     * Si el nuevo valor es igual al anterior retornamos el anterior y de esta manera
+     * no se ejecuta el render() del componente que lo observa.
+     * Esto es performante solo en algunos casos, a veces ejecutar el render() es menos costoso.
+     * En este caso conviene porque es un array con 2 strings.
+     * Vease: https://facebook.github.io/react-native/docs/performance.html#js-fps-plunges-when-re-rendering-a-view-that-hardly-changes
+     * "if you have to do a deep comparison of a large list of objects, it may be that re-rendering your entire component would be quicker, and it would certainly require less code."
+     * 
+     * Tambien se puede usar: comparer.identity, comparer.default o comparer.structural de Mobx
+     */
+    @computed({ equals: arrayEquals }) get filteredTodos() {
         const matchesFilter = new RegExp(this.filter, 'i');
-        let filtered = this.todos.filter(todo => !this.filter || matchesFilter.test(todo));
-
-        // Start Check
-        // Si el nuevo valor es igual al anterior retornamos el anterior y de esta manera
-        // no se ejecuta el render() del componente que lo observa.
-        // Esto es performante solo en algunos casos, a veces ejecutar el render() es menos costoso.
-        // En este caso conviene porque es un array con 2 strings.
-        // Vease: https://facebook.github.io/react-native/docs/performance.html#js-fps-plunges-when-re-rendering-a-view-that-hardly-changes
-        // "if you have to do a deep comparison of a large list of objects, it may be that re-rendering your entire component would be quicker, and it would certainly require less code."
-        if (filtered.length == this.previousFilteredTodos.length) {
-            for (let i = 0; i < filtered.length; i++) {
-                if (filtered[i] != this.previousFilteredTodos[i]) {
-                    break;
-                }
-
-                if (i == filtered.length - 1) {
-                    filtered = this.previousFilteredTodos;
-                }
-            }
-        }
-
-        this.previousFilteredTodos = filtered;
-        // End Check
-
-        return filtered;
+        return this.todos.filter(todo => !this.filter || matchesFilter.test(todo));
     }
 
     @observable secondFilter = '';
